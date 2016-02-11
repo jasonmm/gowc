6fcd0bade6614dc7140bda875e614ae3e6c00ad6
Jason <jasonmm.github@gmail.com>
2016-02-10 18:26:21 -0600

    Added a new test.

--- libgowc/libgowc_test.go
+++ libgowc/libgowc_test.go
@@ -20,3 +20,16 @@ func TestProcessSingleFile(t *testing.T) {
 	}
 }
 
+func TestProcessFiles(t *testing.T) {
+	paths := []string{"./test-file.txt"}
+	m := ProcessFiles(paths)
+	if m.Lines != 3 {
+		t.Errorf("Line count did not equal 3. Line count was %d instead.", m.Lines)
+	}
+	if m.Words != 38 {
+		t.Errorf("Word count did not equal 38.  Word count was %d instead.", m.Words)
+	}
+	if m.Chars != 246 {
+		t.Errorf("Char count did not equal 246.  Char count was %d instead.", m.Chars)
+	}
+}
